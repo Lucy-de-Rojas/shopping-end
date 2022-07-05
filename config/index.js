45b397e07979f75b3a0e2a707c7313a2d3b38e6b
Lucy de Rojas <lucyderojas@gmail.com>
2022-07-05 14:07:29 +0100

    first

---
+++ config/index.js
@@ -0,0 +1,6 @@
+export const db = {
+  DATABASE_URL: process.env.DATABASE_URL,
+};
+
+
+// postgres://pcirqgavxpwacg:b6e1908977aeea8b9243248848e5bcb9ab524216eb632d597c73ed70992d42da@ec2-3-248-121-12.eu-west-1.compute.amazonaws.com:5432/dbfocc64tmhd7i
